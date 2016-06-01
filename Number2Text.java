7e73b0974ac1c3ede0ddf41445d0b920e4b29233
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2016-06-01 10:58:19 -0400

    Fix bugs

--- Number2Text.java
+++ Number2Text.java
@@ -1,61 +1,82 @@
+/**
+ * Prints out text representation of an integer. For example,
+ * 333: three hundred thirty-three
+ *
+ * @author Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
+ * @version 0.1, 06/01/2016
+ */
+
 public class Number2Text {
-  static String[] ones = { "",
-                    "one",
-                    "two",
-                    "three",
-                    "four",
-                    "five",
-                    "six",
-                    "seven",
-                    "eight",
-                    "nine",
-                    "ten",
-                    "eleven",
-                    "twelve",
-                    "thirteen",
-                    "forteen",
-                    "fifteen",
-                    "sixteen",
-                    "seventeen",
-                    "eighteen",
-                    "nineteen",
-                  };
-
-  static String[] tens = { "", "", "twenty",
-                    "thirty",
-                    "forty",
-                    "fifty",
-                    "sixty",
-                    "seventy",
-                    "eighty",
-                    "ninety",
-                  };
-
-  static String[] big = {
-                    "",
-                    "thousand",
-                    "million",
-                    "trillion",
-                    "quadrillion",
-                    "quintillion",
-                    "sextillion",
-                  };
 
   public static void main(String[] args) {
-    String numberStr = "";
-    String hundred = " hundred ";
-    String toAdd = "";
-    int numberInt = Integer.parseInt(args[0]);
-    int a, b, c, t;
 
+    if (args.length != 1) {
+      System.out.println("Usage: java Number2Text [integer]\nAborting.");
+  		return;
+  	}
+
+    int n = Integer.parseInt(args[0]);
+
+    String[] ones = { "",
+                      "one",
+                      "two",
+                      "three",
+                      "four",
+                      "five",
+                      "six",
+                      "seven",
+                      "eight",
+                      "nine",
+                      "ten",
+                      "eleven",
+                      "twelve",
+                      "thirteen",
+                      "forteen",
+                      "fifteen",
+                      "sixteen",
+                      "seventeen",
+                      "eighteen",
+                      "nineteen",
+                    };
+
+    String[] tens = { "",   // first 20 numbers are hardcoded
+                      "",   // in ones[] above
+                      "twenty",
+                      "thirty",
+                      "forty",
+                      "fifty",
+                      "sixty",
+                      "seventy",
+                      "eighty",
+                      "ninety",
+                    };
+
+    String[] huge = { "",   // nothing for integers <= 999
+                      "thousand",
+                      "million",
+                      "trillion",
+                      "quadrillion",
+                      "quintillion",
+                      "sextillion",
+                    };
+
+    String nStr = "";
+    String hundred = " hundred ";
+    String piece = "";
+
+    int a, b, c, t;
     int triple = 0;
 
-    while (numberInt > 0) {
-      t = numberInt % 1000;
-      numberInt /= 1000;
+    if (n == 0) {
+      nStr = "zero";
+    }
+
+    while (n > 0) {
+      t = n % 1000;
+      n /= 1000;
 
       if (t < 20) {
-        toAdd = ones[t];
+        piece = ones[t];
       } else {
         a = t / 100;
         b = (t - a * 100) / 10;
@@ -63,21 +84,28 @@ public class Number2Text {
 
         if (a > 0) {
           if (10 * b + c < 20) {
-            toAdd = ones[a] + hundred + ones[10 * b + c];
+            piece = ones[a] + hundred + ones[10 * b + c];
           } else {
-            toAdd = ones[a] + hundred + tens[b] + "-" + ones[c];
+            piece = ones[a] + hundred + tens[b] + "-" + ones[c];
           }
         } else {
-          toAdd = tens[b] + "-" + ones[c];
+          piece = tens[b] + "-" + ones[c];
         }
       }
 
-      numberStr = toAdd + " " + big[triple] + " " + numberStr;
+      if (t == 0) {
+        nStr = piece + " " + nStr;
+      } else {
+        nStr = piece + " " + huge[triple] + " " + nStr;
+      }
 
       triple++;
     }
 
-    System.out.print(numberStr + "\n");
+    // Removing extra spaces
+    nStr = nStr.trim().replaceAll(" +", " ");
+
+    System.out.print(nStr + "\n");
   }
 
 }
